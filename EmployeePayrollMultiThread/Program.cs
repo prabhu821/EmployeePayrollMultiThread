bd48540c47383add16d4bffd329815a6a64c3ef6
rishiraj007 <rishiraj@mounttalent.com>
2023-05-01 18:40:24 +0530

    [Prabhu]Add: Employee Payroll Multi Thread

---
+++ EmployeePayrollMultiThread/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayrollMultiThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Payroll Multi Thread!");
+        }
+    }
+}
